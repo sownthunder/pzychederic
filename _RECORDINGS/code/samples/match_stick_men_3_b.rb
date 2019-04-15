d9b85fcb8c7476fb9be24f35185101adbda13c5f
sownthunder <dbates117@gmail.com>
2019-04-15 18:31:36 -0400

    addition of more files

---
+++ _RECORDINGS/code/samples/match_stick_men_3_b.rb
@@ -0,0 +1,38 @@
+# Welcome to Sonic Pi v3.1
+
+box_samples = "/Users/dbates/Box/"
+# status quo - pictures of matchstick men - enhanced sound.wav
+
+#############################################
+
+set_volume! 2
+
+use_bpm 100
+
+2.times do
+  3.times do
+    with_fx :flanger, mix: 0.7 do
+      with_fx :bitcrusher, mix: 0.8, pre_amp: 4 do
+        
+        sample box_samples, "status quo - pictures of matchstick men - enhanced sound.wav", amp: 2,  beat_stretch: 35, pitch: -12
+        sleep 21
+      end
+    end
+  end
+  #sample :loop_amen_full, beat_stretch: 16, start: 0.75
+  #sleep 4
+end
+
+loop :testLoop
+
+"
+2.times do
+  3.times do
+    sample :loop_amen, beat_stretch: 4
+    sleep 4
+  end
+  sample :loop_amen_full, beat_stretch: 16, start: 0.75
+  sleep 4
+end
+"
+
